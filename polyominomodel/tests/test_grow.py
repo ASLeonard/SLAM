bb3e7bd909db915a17575a5651198a411484fd28
Alex Leonard <tkom343@gmail.com>
2017-10-09 10:10:02 +0100

    del old dir

--- polyominomodel/tests/test_grow.py
+++
@@ -1,8 +0,0 @@
-print "Loading module"
-import polyominomodel as pm
-print "Growing unlabelled"
-pm.GrowPoly([1,1,1,1,2,0,0,0],0)
-print "Growing labelled"
-pm.GrowPoly([1,1,1,1,2,0,0,0],0)
-print "Saving gif"
-pm.GrowPoly([1,1,1,1,2,0,0,0],0,"Test_Save")
